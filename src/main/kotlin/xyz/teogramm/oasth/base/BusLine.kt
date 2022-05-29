65c6da314103c2421549459ac7295fcc0dc33d0a
Theodoros Grammenos <teogramm@outlook.com>
2022-05-29 11:43:41 +0300

    [BusLine] Add methods for getting routes

--- src/main/kotlin/xyz/teogramm/oasth/base/BusLine.kt
+++ src/main/kotlin/xyz/teogramm/oasth/base/BusLine.kt
@@ -13,6 +13,25 @@ class BusLine(
     val routes: List<BusRoute>,
     val schedules: Set<BusSchedule>
 ){
+
+    fun getCircularRoutes(): List<BusRoute>{
+        return getRoutesByType(RouteTypes.CIRCULAR)
+    }
+
+    fun getInboundRoutes(): List<BusRoute>{
+        return getRoutesByType(RouteTypes.INBOUND)
+    }
+
+    fun getOutboundRoutes(): List<BusRoute>{
+        return getRoutesByType(RouteTypes.OUTBOUND)
+    }
+
+    private fun getRoutesByType(type: RouteTypes): List<BusRoute>{
+        return routes.filter { route ->
+            route.type == type
+        }
+    }
+
     override fun toString(): String {
         return "Line $number - $nameEN"
     }
